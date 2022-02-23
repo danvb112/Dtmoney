1225adb48525bd4a366332e6be22f66ce833cf2c
danvb112 <danielbonasser@gmail.com>
2022-02-23 07:09:08 -0300

    Create TransactionsContext

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header/Header";
 import { GlobalStyle } from "./styles/global";
 import {NewTransactionModal} from './components/NewTransactionModal/NewTransactionModal';
+import { TransactionsProvider } from './TransactionsContext';
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
@@ -17,7 +18,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -27,6 +28,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
--- src/components/TransactionTable/TransactionTable.tsx
+++ src/components/TransactionTable/TransactionTable.tsx
@@ -1,23 +1,11 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useContext} from "react";
+import { TransactionContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
-interface Transaction {
-    id: number;
-    title: string;
-    type: string;
-    category: string;
-    amount: number;
-    createdAt: string;
-}
-
 export function TransactionTable() {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
+    const transactions = useContext(TransactionContext);
 
-    useEffect(() => {
-        api('transactions')
-            .then(response => setTransactions(response.data.transactions))
-    }, []);
+    console.log(transactions);
 
     return (
         <Container>
